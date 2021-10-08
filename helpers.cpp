a0c148f0a342b33fd7c274e50ab1fef15ef4042b
sharan21 <sharan.n21@gmail.com>
2021-10-08 23:08:30 +0530

    added http server with fdset

---
+++ helpers.cpp
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <iostream>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string>
+#include <map>
+
+using namespace std;
+
+void print_request(map<string, string> req){
+    for(auto item: req){
+        cout << item.first << ":" << item.second << endl;
+    }
+}
+
+map<string, string> parse_header( void *msg )
+{
+
+    map<string, string> http_request;
+    char *head = (char *) msg;
+    char *mid;
+    char *tail = head;
+
+    if( sizeof( msg ) == 0 ){
+        return http_request;
+    }
+
+    // Find request type
+    while( *head++ != ' ');
+    http_request[ "Type" ] = string( ( char * ) msg ).substr( 0 , ( head - 1) - tail );
+
+    // Find path
+    tail = head;
+    while( *head++ != ' ');
+    http_request[ "Path" ] = string( ( char * ) msg ).substr( tail - ( char *)msg , ( head - 1) - tail );
+
+    // Find HTTP version
+    tail = head;
+    while( *head++ != '\r');
+    http_request[ "Version" ] = string( ( char * ) msg ).substr( tail - ( char *)msg , ( head - 1) - tail );
+
+    // Map all headers from a key to a value
+    while( true )
+    {
+        tail = head + 1;
+        while( *head++ != '\r' );
+        mid = strstr( tail, ":" );   
+
+        // Look for the failed strstr   
+        if( tail > mid )
+            break;
+
+        http_request[ string( ( char * ) msg ).substr( tail - ( char *)msg , ( mid ) - tail  ) ] = string( ( char * ) msg ).substr( mid + 2 - ( char *) msg , ( head - 3 ) - mid );
+    }
+    return http_request;
+}
+
+
+void send_file(int socket, char *file, bool fileSwitch=false)
+{
+	char *sendbuf;
+	FILE *requested_file;
+	printf("Received request for file: %s on socket %d\n\n", file + 1, socket);
+	cout << file << endl;
+	if (fileSwitch) { requested_file = fopen(file + 1, "rb"); }
+	else { requested_file = fopen(file + 1, "r"); }
+	
+	if (requested_file == NULL){
+		cout << "404 " << endl;
+		
+	}
+	else 
+	{
+		printf("Hit else #1\n");
+		fseek (requested_file, 0, SEEK_END);
+		int fileLength = ftell(requested_file);
+		rewind(requested_file);
+		
+		sendbuf = (char*) malloc (sizeof(char)*fileLength);
+		size_t result = fread(sendbuf, 1, fileLength, requested_file);
+		
+		if (result > 0) {
+            write(socket , sendbuf , result);
+			// send(socket, sendbuf, result, 0);		
+		}		
+		else { printf("Send error."); exit(1); }
+	}
+	
+	fclose(requested_file);
+}
--- http-server.cpp
+++ http-server.cpp
@@ -1,127 +1,127 @@
-// Server side C program to demonstrate HTTP Server programming
-#include <stdio.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <iostream>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <string>
-#include <map>
+#include "helpers.cpp"
+#define max_buf_size 5120
+#define server_port 4950
+#define max_n_threads 1023
 using namespace std;
 
+string content = "<!DOCTYPE html><html><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link rel=\"stylesheet\" href=\"/w3css/3/w3.css\"><link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css\"><body><!-- Navigation --><nav class=\"w3-bar w3-black\">  <a href=\"#home\" class=\"w3-button w3-bar-item\">Home</a>  <a href=\"#band\" class=\"w3-button w3-bar-item\">Band</a>  <a href=\"#tour\" class=\"w3-button w3-bar-item\">Tour</a>  <a href=\"#contact\" class=\"w3-button w3-bar-item\">Contact</a></nav><!-- Slide Show --><section>  <img class=\"mySlides\" src=\"img_band_la.jpg\"  style=\"width:100%\">  <img class=\"mySlides\" src=\"img_band_ny.jpg\"  style=\"width:100%\">  <img class=\"mySlides\" src=\"img_band_chicago.jpg\"  style=\"width:100%\"></section><!-- Band Description --><section class=\"w3-container w3-center w3-content\" style=\"max-width:600px\">  <h2 class=\"w3-wide\">THE BAND</h2>  <p class=\"w3-opacity\"><i>We love music</i></p>  <p class=\"w3-justify\">We have created a fictional band website. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p></section><!-- Band Members --><section class=\"w3-row-padding w3-center w3-light-grey\">  <article class=\"w3-third\">    <p>John</p>    <img src=\"img_bandmember.jpg\" alt=\"Random Name\" style=\"width:100%\">    <p>John is the smartest.</p>  </article>  <article class=\"w3-third\">    <p>Paul</p>    <img src=\"img_bandmember.jpg\" alt=\"Random Name\" style=\"width:100%\">    <p>Paul is the prettiest.</p>  </article>  <article class=\"w3-third\">    <p>Ringo</p>    <img src=\"img_bandmember.jpg\" alt=\"Random Name\" style=\"width:100%\">    <p>Ringo is the funniest.</p>  </article></section><!-- Footer --><footer class=\"w3-container w3-padding-64 w3-center w3-black w3-xlarge\">  <a href=\"#\"><i class=\"fa fa-facebook-official\"></i></a>  <a href=\"#\"><i class=\"fa fa-pinterest-p\"></i></a>  <a href=\"#\"><i class=\"fa fa-twitter\"></i></a>  <a href=\"#\"><i class=\"fa fa-flickr\"></i></a>  <a href=\"#\"><i class=\"fa fa-linkedin\"></i></a>  <p class=\"w3-medium\">  Powered by <a href=\"https://www.w3schools.com/w3css/default.asp\" target=\"_blank\">w3.css</a>  </p></footer><script>// Automatic Slideshow - change image every 3 secondsvar myIndex = 0;carousel();function carousel() {  var i;  var x = document.getElementsByClassName(\"mySlides\");  for (i = 0; i < x.length; i++) {     x[i].style.display = \"none\";  }  myIndex++;  if (myIndex > x.length) {myIndex = 1}  x[myIndex-1].style.display = \"block\";  setTimeout(carousel, 3000);}</script></body></html>";
+string msg = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length:" + to_string(content.length()) + "\n\n" + content;
 
+char receiver_buffer[max_buf_size];
+int curr_no_of_clients = 0;
 
-map<string, string> parse_header( void *msg )
+void service_socket(int client_fd, fd_set *all_fds, int server_soc_fd, int last_fd)
 {
-
-    map<string, string> http_request;
-    char *head = (char *) msg;
-    char *mid;
-    char *tail = head;
-
-    if( sizeof( msg ) == 0 )
-    {
-        return http_request;
-    }
-
-    // Find request type
-    while( *head++ != ' ');
-    http_request[ "Type" ] = std::string( ( char * ) msg ).substr( 0 , ( head - 1) - tail );
-
-    // Find path
-    tail = head;
-    while( *head++ != ' ');
-    http_request[ "Path" ] = std::string( ( char * ) msg ).substr( tail - ( char *)msg , ( head - 1) - tail );
-
-    // Find HTTP version
-    tail = head;
-    while( *head++ != '\r');
-    http_request[ "Version" ] = std::string( ( char * ) msg ).substr( tail - ( char *)msg , ( head - 1) - tail );
-
-    // Map all headers from a key to a value
-    while( true )
-    {
-        tail = head + 1;
-        while( *head++ != '\r' );
-        mid = strstr( tail, ":" );   
-
-        // Look for the failed strstr   
-        if( tail > mid )
-            break;
-
-        http_request[ std::string( ( char * ) msg ).substr( tail - ( char *)msg , ( mid ) - tail  ) ] = std::string( ( char * ) msg ).substr( mid + 2 - ( char *) msg , ( head - 3 ) - mid );
-    }
-
-    
-    return http_request;
+	int rec_buffer_size;	
+	if ((rec_buffer_size = recv(client_fd, receiver_buffer, max_buf_size, 0)) == 0)
+	{ //client wants to close connection
+		close(client_fd);
+		FD_CLR(client_fd, all_fds); //set 0 to bit corresponding to this clients fd
+		curr_no_of_clients--;
+	}
+	else
+	{   
+        map<string, string> request = parse_header(receiver_buffer);
+        // print_request(request);
+        cout << "serving " << request["Path"] << " to client with port " << client_fd << endl;
+        send(client_fd, msg.c_str(), strlen(msg.c_str()), 0);	
+		memset(receiver_buffer, 0, max_buf_size);
+	}
 }
 
-using namespace std;
-
-#define PORT 8080
-int main(int argc, char const *argv[])
+void accept_client_connection(fd_set *all_fds, int *last_fd, int server_soc_fd, struct sockaddr_in client_addr)
 {
-    int server_fd, new_socket; long valread;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-    
-    // Only this line has been changed. Everything is same.
-    string content = "<!DOCTYPE html><html><head><title>Example</title></head><body><p>This is an example of a simple HTML page with one paragraph.</p></body></html>";
-    string hello = "HTTP/1.1 200 OK\nContent-Type: text/html\nContent-Length:" + to_string(content.length()) + "\n\n" + content;
+	int client_fd;
+	socklen_t client_addr_len;
+	client_addr_len = sizeof(struct sockaddr_in);
 
-    
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("In socket");
-        exit(EXIT_FAILURE);
-    }
-    
+	if (curr_no_of_clients == max_n_threads)
+	{
+		cout << "Reached Max no of clients, cannot accept more!" << endl;
+		return;
+	}
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-    
-    memset(address.sin_zero, '\0', sizeof address.sin_zero);
-    
-    
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
-    {
-        perror("In bind");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 10) < 0)
-    {
-        perror("In listen");
-        exit(EXIT_FAILURE);
-    }
-    while(1)
-    {
-        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
-        {
-            perror("In accept");
-            exit(EXIT_FAILURE);
-        }
-        
-        char buffer[30000] = {0};
-        valread = read(new_socket, buffer, 30000);
-        // cout << buffer << endl;
-        map<string, string> http_request = parse_header(buffer);
+	if ((client_fd = accept(server_soc_fd, (struct sockaddr *)&client_addr, &client_addr_len)) < 0)
+	{
+		cout << "Failed to accept client" << endl;
+		exit(1);
+	}
+	else
+	{
+		FD_SET(client_fd, all_fds); //set the client fd to 1
+		curr_no_of_clients++;
 
-        // Determine if successful
-        std::cout << http_request[ "Host" ] << std::endl; 
-        std::cout << http_request[ "Upgrade-Insecure-Requests" ] << std::endl; 
-        std::cout << http_request[ "Accept" ] << std::endl; 
-        std::cout << http_request[ "User-Agent" ] << std::endl; 
-        std::cout << http_request[ "Accept-Language" ] << std::endl; 
-        std::cout << http_request[ "Accept-Encoding" ] << std::endl; 
-        std::cout << http_request[ "Connection" ] << std::endl; 
+		if (client_fd > *last_fd) // update the last fd/ no of active dfs
+			*last_fd = client_fd;
 
+		cout << "connected to new client with port " << client_fd << endl;
+	}
+}
 
-        write(new_socket , hello.c_str() , strlen(hello.c_str()));
-        printf("------------------Hello message sent-------------------");
-        close(new_socket);
-    }
-    return 0;
+int main()
+{
+
+	int flag = 1, server_soc_fd = 0, last_fd, i; // last fd stores the
+	fd_set all_fds, current_fds;
+
+	struct sockaddr_in server_addr, client_addr;
+
+	FD_ZERO(&all_fds);	   //a bit array of all the socket fds
+	FD_ZERO(&current_fds); //a bit array of the servicable fds
+
+	if ((server_soc_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		cout << "Failed to init socket" << endl;
+		exit(1);
+	}
+
+	// INIT SERVER ADDR
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(4950);
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	memset(server_addr.sin_zero, '\0', sizeof server_addr.sin_zero);
+
+	//INIT SOCKET
+	if (setsockopt(server_soc_fd, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(int)) < 0)
+	{
+		cout << " Failed to set socket options" << endl;
+		exit(1);
+	}
+
+	//BIND SOCKET (removed check for c++11)
+	bind(server_soc_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr));
+
+	//START LISTENING
+	if (listen(server_soc_fd, 10) < 0)
+	{
+		cout << "Failed to start listening" << endl;
+		exit(1);
+	}
+
+	cout << "Started listening on port 4950...";
+	cout.flush(); //empty stdout buffer
+
+	FD_SET(server_soc_fd, &all_fds); //set server fd to 1 since server is now active
+	last_fd = server_soc_fd;		 //currently only server fd is present, so last_fd = 1
+
+	while (true)
+	{
+		current_fds = all_fds; //update current set of fds, incase an fd has been added/ closed
+		if (select(last_fd + 1, &current_fds, NULL, NULL, NULL) < 0)
+		{ //once an fd is set, condition will break
+			cout << "error occured during select()" << endl;
+			exit(1);
+		}
+		for (i = 0; i < last_fd + 1; i++)
+		{ //iterate through list of fds
+			if (FD_ISSET(i, &current_fds))
+			{							//check which socket sent data
+				if (i == server_soc_fd) // server accepting new client
+					accept_client_connection(&all_fds, &last_fd, server_soc_fd, client_addr);
+				else
+					service_socket(i, &all_fds, server_soc_fd, last_fd); // a client has sent data to server
+			}
+		}
+	}
+	return 0;
 }
