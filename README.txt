2bd7aaa36a41c17cafdf0cc684efc459f8b655e1
sharan21 <sharan.n21@gmail.com>
2021-10-10 02:20:13 +0530

    done

---
+++ README.txt
@@ -0,0 +1,10 @@
+Requirements:
+    - OpenMP
+
+To run experiments:
+    bash build.sh
+    ./server
+    ./client
+
+To adjust the number of clients, change value in inp-params.txt. Please read the report for more information.
+
--- http-client.cpp
+++ http-client.cpp
@@ -13,9 +13,13 @@ void service_socket(char sender_buffer[], int server_sock_fd)
 int main()
 {
 /*********************** INIT GLOBAL FILES/OBJECTS *************************/
+    long int done = 0;
     int connected_threads = 0, n;
     ifstream input_file;
+    fstream exps_file;
 	input_file.open("inp-params.txt", ios::in);
+    exps_file.open("exps.txt", ios::app);
+
 	input_file >> n;
     cout << "#client: " << n << endl;
     omp_set_num_threads(n);
@@ -28,8 +32,9 @@ int main()
         struct sockaddr_in server_addr;
         struct timeval tv = { 0, 10 }; //after 1 second select() will timeout
         fd_set all_fds, current_fds;
+        #pragma omp barrier
 /********************************* SOCKET CREATION ******************************/
-        usleep(id * 100); //to prevent DDoSsing yourself by sequentially adding threads to server
+        // usleep(id * 100); //to prevent DDoSsing yourself by sequentially adding threads to server
         if ((server_sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
             cout << "failed to create socket" << endl;
             exit(1);
@@ -50,28 +55,55 @@ int main()
         FD_SET(server_sock_fd, &all_fds); //set server fd to 1
         last_fd = server_sock_fd; //we only need to listen to the server port
 /******************************	 SEND AND RECEIVE *********************************/
+if(id == 0)
+    cout << "Waiting for all clients to connect..." << endl;
+#pragma omp barrier // wait for all clients to connect to server
 
 #pragma omp critical
         {
             connected_threads++;
         }
         string msg = "GET /index.html HTTP/1.1 \r\n\r\n";
-    
-        while (true) {    
-            send(server_sock_fd, msg.c_str(), strlen(msg.c_str()), 0); 
-            #pragma omp critical
-            {
-                cout << id << " sent request:" << msg << endl;
-            }   
-            char receiver_buffer[max_buf_size];
-            int message_size = recv(server_sock_fd, receiver_buffer, max_buf_size, 0);
-            receiver_buffer[message_size] = '\0';
-            #pragma omp critical
-            {
-                cout << id << " received object of size: " << message_size << endl;
-            }    
-            usleep(1000);
+        float mean;
+        if(id == 0){ //thread to compute and log statistics
+            cout << "All clients connected! #clients: " << n << endl;
+            int sec = 0;
+            while(true){
+                long int t1 = done;
+                sleep(1);
+                mean += done - t1;
+                cout << done - t1 << " responses/second" << endl;
+                sec ++;
+                if(sec % 10 == 0){
+                    cout << "------ Mean requests processed per 10 seconds: " <<  mean/10 << " ------" << endl;
+                    mean = 0;
+                }
+            }
         }
+        else{ // client threads
+            while (true) {    
+                send(server_sock_fd, msg.c_str(), strlen(msg.c_str()), 0); 
+                // #pragma omp critical
+                // {
+                //     cout << id << " sent request:" << msg << endl;
+                // }   
+                char receiver_buffer[max_buf_size];
+                int message_size = recv(server_sock_fd, receiver_buffer, max_buf_size, 0);
+                receiver_buffer[message_size] = '\0';
+                // #pragma omp critical
+                // {
+                //     cout << id << " received object of size: " << message_size << endl;
+                // }    
+                #pragma omp critical
+                {
+                    done += 1;
+                }
+                usleep(1000);
+            }
+
+        }
+    
+       
     } 
     return 0;
 }
